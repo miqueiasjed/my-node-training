73441581c58a0e67b28f461b9a09a3b5aa2a5bf0
Miqueias Mesquita <miqueias_jed@hotmail.com>
2024-12-03 12:07:00 -0300

    fs module

--- pacotes/projeto2/index.js
+++ pacotes/projeto2/index.js
@@ -1,7 +1,10 @@
 import pensador from "pensador-scrap";
+import fs from "fs";
+
+const autor = "Platao";
 
 const {error, success} = await pensador.search({
-  query: "Platao",
+  query: autor,
   limit: 12
 });
 
@@ -9,9 +12,15 @@ const {error, success} = await pensador.search({
 // console.log(error);
 
 const pensamentos = success.thought;
-const indiceAleatorio = Math.floor(Math.random() * pensamentos.length)
-const pensamentoSelecionado = pensamentos[indiceAleatorio];
+const indiceAleatorio = Math.floor(Math.random() * pensamentos.length);
+const pensamentoSelecionado = pensamentos[indiceAleatorio].content;
 
-console.log(pensamentoSelecionado.content);
+const pensamentoSalvar = `Pensamento de ${autor} -> ${pensamentoSelecionado}\n`;
 
+// fs.writeFileSync("frases.txt", pensamentoSalvar, "utf8");
+fs.appendFileSync("frases.txt", pensamentoSalvar, "utf8");
+
+const texto = fs.readFileSync("frases.txt", "utf8");
+
+console.log(texto);
 
