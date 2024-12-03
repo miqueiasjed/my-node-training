c46e6a235c1bcd7b1d7a38c2d80d623e13389abc
Miqueias Mesquita <miqueias_jed@hotmail.com>
2024-12-03 12:30:13 -0300

    errors

--- pacotes/projeto2/index.js
+++ pacotes/projeto2/index.js
@@ -2,7 +2,7 @@ import pensador from "pensador-scrap";
 import fs from "fs";
 
 const autor = "Platao";
-
+let pensamentoSalvar;
 const {error, success} = await pensador.search({
   query: autor,
   limit: 12
@@ -11,16 +11,21 @@ const {error, success} = await pensador.search({
 // console.log(success);
 // console.log(error);
 
-const pensamentos = success.thought;
-const indiceAleatorio = Math.floor(Math.random() * pensamentos.length);
-const pensamentoSelecionado = pensamentos[indiceAleatorio].content;
+try{
+  const pensamentos = success.thought;
+  const indiceAleatorio = Math.floor(Math.random() * pensamentos.length);
+  const pensamentoSelecionado = pensamentos[indiceAleatorio].content;
 
-const pensamentoSalvar = `Pensamento de ${autor} -> ${pensamentoSelecionado}\n`;
+  pensamentoSalvar = `Pensamento de ${autor} -> ${pensamentoSelecionado}\n`;
+}catch(erro){
+  pensamentoSalvar = `o ${autor} não tem pensamento\n`
+}finally{
+  // fs.writeFileSync("frases.txt", pensamentoSalvar, "utf8");
+  fs.appendFileSync("frases.txt", pensamentoSalvar, "utf8");
 
-// fs.writeFileSync("frases.txt", pensamentoSalvar, "utf8");
-fs.appendFileSync("frases.txt", pensamentoSalvar, "utf8");
+  const texto = fs.readFileSync("frases.txt", "utf8");
 
-const texto = fs.readFileSync("frases.txt", "utf8");
+  console.log(texto);
+}
 
-console.log(texto);
 
